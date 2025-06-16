17a301040f51de2bb7c6b3d2d3d8543902e3367b
Guillaume <goldnord.digital@mail.com>
2025-06-16 15:00:50 +0200

    changement ordernumber facture

--- pages/api/transactions.js
+++ pages/api/transactions.js
@@ -155,7 +155,7 @@ export default async function handler(req, res) {
         // Upload de la facture
         const factureUploadResult = await uploadInvoiceToDrive(
           facturePDF,
-          `facture_${factureNumber}.pdf`,
+          `facture_${orderNumber}.pdf`,
           transactionLocation,
           orderNumber,
           clientName
@@ -255,7 +255,7 @@ Détails de la transaction :
 Cordialement,
 GOLD NORD`,
           facturePDF,
-          `facture_${factureNumber}.pdf`
+          `facture_${orderNumber}.pdf`
         );
       }
 
