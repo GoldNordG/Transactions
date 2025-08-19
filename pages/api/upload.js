0a42b3415e0ad3c869b10d6869e1e1b27d6ea343
Guillaume <goldnord.digital@mail.com>
2025-08-19 15:22:29 +0200

    Mise à jour

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -3,7 +3,6 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { compare } from "bcrypt";
-import prisma from "../../../lib/prisma.js";
 
 export const runtime = "nodejs";
 
